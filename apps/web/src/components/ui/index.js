6f5bbdfd4da31406501743d599b6ec9d3ab02809
Hedun20 <99669256+Hedun20@users.noreply.github.com>
2026-07-09 19:29:04 +0200

    Stage 10E: add UI block exports

---
+++ apps/web/src/components/ui/index.js
@@ -0,0 +1,8 @@
+export { default as ActionRow } from "./ActionRow.jsx";
+export { default as CodexButton } from "./CodexButton.jsx";
+export { default as CodexCard } from "./CodexCard.jsx";
+export { default as EmptyState } from "./EmptyState.jsx";
+export { default as PageHero } from "./PageHero.jsx";
+export { default as PageShell } from "./PageShell.jsx";
+export { default as SectionHeader } from "./SectionHeader.jsx";
+export { default as StatusMessage } from "./StatusMessage.jsx";
