59868352f71c0e7ccf3413f05cf0b53402e585a1
Hedun20 <99669256+Hedun20@users.noreply.github.com>
2026-07-20 19:24:43 +0200

    feat(branding): route campaign timeline page

--- apps/web/branding/src/BrandingApp.jsx
+++ apps/web/branding/src/BrandingApp.jsx
@@ -5,6 +5,7 @@ import CampaignArchivePage from "./pages/CampaignArchivePage.jsx";
 import LoreEntryPage from "./pages/LoreEntryPage.jsx";
 import CreateEntryPage from "./pages/CreateEntryPage.jsx";
 import NpcRosterPage from "./pages/NpcRosterPage.jsx";
+import CampaignTimelinePage from "./pages/CampaignTimelinePage.jsx";
 import CharacterDossierPage from "./pages/CharacterDossierPage.jsx";
 import DiceWorkspacePage from "./pages/DiceWorkspacePage.jsx";
 import InvitationsPage from "./pages/InvitationsPage.jsx";
@@ -19,6 +20,7 @@ export default function BrandingApp() {
         <Route path="/entry" element={<LoreEntryPage />} />
         <Route path="/entry/new" element={<CreateEntryPage />} />
         <Route path="/npcs" element={<NpcRosterPage />} />
+        <Route path="/timeline" element={<CampaignTimelinePage />} />
         <Route path="/character" element={<CharacterDossierPage />} />
         <Route path="/dice" element={<DiceWorkspacePage />} />
         <Route path="/invitations" element={<InvitationsPage />} />
