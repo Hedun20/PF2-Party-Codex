521f320f40408f8d143f8254d62a6f81b04e9cc6
Hedun20 <99669256+Hedun20@users.noreply.github.com>
2026-07-20 13:14:04 +0200

    Add branding lab routes

---
+++ apps/web/branding/src/BrandingApp.jsx
@@ -0,0 +1,21 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import BrandingShell from "./components/BrandingShell.jsx";
+import FoundationsPage from "./pages/FoundationsPage.jsx";
+import CharacterDossierPage from "./pages/CharacterDossierPage.jsx";
+import DiceWorkspacePage from "./pages/DiceWorkspacePage.jsx";
+import InvitationsPage from "./pages/InvitationsPage.jsx";
+
+export default function BrandingApp() {
+  return (
+    <BrandingShell>
+      <Routes>
+        <Route index element={<Navigate to="/foundations" replace />} />
+        <Route path="/foundations" element={<FoundationsPage />} />
+        <Route path="/character" element={<CharacterDossierPage />} />
+        <Route path="/dice" element={<DiceWorkspacePage />} />
+        <Route path="/invitations" element={<InvitationsPage />} />
+        <Route path="*" element={<Navigate to="/foundations" replace />} />
+      </Routes>
+    </BrandingShell>
+  );
+}
