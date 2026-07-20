588ab1af383cf32bc4c8eec02eabb9f46fd628ca
Hedun20 <99669256+Hedun20@users.noreply.github.com>
2026-07-20 18:57:47 +0200

    Branding: route Campaign Archive composition

--- apps/web/branding/src/BrandingApp.jsx
+++ apps/web/branding/src/BrandingApp.jsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import BrandingShell from "./components/BrandingShell.jsx";
 import FoundationsPage from "./pages/FoundationsPage.jsx";
+import CampaignArchivePage from "./pages/CampaignArchivePage.jsx";
 import CharacterDossierPage from "./pages/CharacterDossierPage.jsx";
 import DiceWorkspacePage from "./pages/DiceWorkspacePage.jsx";
 import InvitationsPage from "./pages/InvitationsPage.jsx";
@@ -11,6 +12,7 @@ export default function BrandingApp() {
       <Routes>
         <Route index element={<Navigate to="/foundations" replace />} />
         <Route path="/foundations" element={<FoundationsPage />} />
+        <Route path="/archive" element={<CampaignArchivePage />} />
         <Route path="/character" element={<CharacterDossierPage />} />
         <Route path="/dice" element={<DiceWorkspacePage />} />
         <Route path="/invitations" element={<InvitationsPage />} />
