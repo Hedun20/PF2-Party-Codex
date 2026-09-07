a90d0458bfdfbf42408c99ed24e09c222a2c06ea
Hedun20 <99669256+Hedun20@users.noreply.github.com>
2026-09-07 14:37:39 +0200

    feat(ingestion): export session ingestion contracts

--- packages/contracts/src/index.ts
+++ packages/contracts/src/index.ts
@@ -10,5 +10,6 @@ export * from "./jobs.js";
 export * from "./migration.js";
 export * from "./policy.js";
 export * from "./session.js";
+export * from "./sessionIngestion.js";
 export * from "./sessionWorker.js";
 export * from "./validation.js";
