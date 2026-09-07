ada147fe90c71460d15bc7a0b732cb474dfcbb10
Hedun20 <99669256+Hedun20@users.noreply.github.com>
2026-09-07 12:40:49 +0200

    feat(sessions): export worker processing contract

--- packages/contracts/src/index.ts
+++ packages/contracts/src/index.ts
@@ -10,4 +10,5 @@ export * from "./jobs.js";
 export * from "./migration.js";
 export * from "./policy.js";
 export * from "./session.js";
+export * from "./sessionWorker.js";
 export * from "./validation.js";
